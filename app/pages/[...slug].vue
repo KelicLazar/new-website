0bab485ea7bff4c8a12ce4ff2c3dbb97db7fad6b
Lazar Kelić <keliclazar@gmail.com>
2025-07-23 21:44:12 +0200

    das

---
+++ app/pages/[...slug].vue
@@ -0,0 +1,34 @@
+<script setup>
+// const appConfig = useAppConfig();
+const route = useRoute();
+// console.log(route.path, route);
+const { data: val } = await useAsyncData(route.path, () =>
+  queryCollection("content").path(route.path).first()
+);
+// This will handle all dynamic pages from your content
+
+const nuxtApp = useNuxtApp();
+
+if (nuxtApp.ssrContext) {
+  console.log("Running on the server");
+} else {
+  console.log("Running on the client");
+}
+
+console.log("VAL", route.path, val.value);
+// console.log(appConfig);
+</script>
+
+<template>
+  <div class="bg-red-500">
+    <br /><br />
+    <br /><br />
+    <br /><br />
+    <br /><br />
+    <!-- <ContentRenderer v-if="val" :value="val" /> -->
+    <div>
+      not working not workings not working not working not working not working
+      not working not working not working
+    </div>
+  </div>
+</template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
+  compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
-  modules: ['@nuxt/content']
-})
+  modules: ["@nuxt/content"],
+  content: {
+    preview: {
+      api: "https://api.nuxt.studio",
+      dev: true,
+    },
+  },
+});
